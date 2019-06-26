59db2510af8aa2bf79fe0e3aeaf8dd67731e152c
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-26 08:53:39 +0200

    Toegevoegd

--- volgende.php
+++ volgende.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Stukkie tekst neef, Ik heb deze tekst zojuist helemaal aangepast En nu nog een keer";
+echo "Hallo ik heb nu text aangepast, Ik heb deze tekst zojuist helemaal aangepast En nu nog een keer";
 
 ?>
