952a56dfd3222b3a4e0f97f34a7f1c92a9897ca8
Olaf Krommendijk <oh.krommendijk@student.alfa-college.nl>
2019-06-26 09:16:06 +0200

    Spelling verbeterd

--- volgende.php
+++ volgende.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hallo ik heb nu text aangepast, Ik heb deze tekst zojuist helemaal aangepast En nu nog een keer";
+echo "Hallo ik heb nu text aangepast, ik heb deze tekst zojuist helemaal aangepast. En nu nog een keer.";
 
 ?>
